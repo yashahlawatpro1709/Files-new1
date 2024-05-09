58b404191d372733e447a939c07d15529690e7f2
YASH AHLAWAT <yashahlawat@yash.ahlawat-C02DT1KNMD6N>
2024-05-09 13:37:56 +0530

    files

---
+++ app/page.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import Cards from "./components/Cards";
+import Card from "./components/Card";
+import Pagedata from "./components/Pagedata";
+import Pagedata1 from "./components/Pagedata1";
+import Suites from "./components/Suites";
+import Customer from "./components/Customer";
+import Supercharge from "./components/Supercharge";
+import Press from "./components/Socialproof";
+import VerticalCarousel from "./components/IconicBrands";
+import Hero from "./components/Hero";
+import Data from "./components/Data";
+import Footer from "./components/Footer";
+
+
+const page = () => {
+  return (
+    <div>
+      <Hero/>
+      <Press/>
+      <Supercharge/>
+      <Customer/>
+      <Suites/>
+      <VerticalCarousel/>
+      <Pagedata1 />
+
+
+      <h2 className="py-20 whitespace-pre-line max-w-mobile-limit tablet:max-w-tablet-limit desktop-sm:max-w-desktop-small-limit desktop-lg:max-w-limit text-center text-mobile/heading-3 tablet:text-tablet/heading-3 desktop-lg:text-desktop/heading-3 dark:text-white font-bold text-4xl">
+        See what analysts and your peers are saying
+      </h2>
+      <Cards />
+      <Data/>
+      <Footer/>
+    </div>
+  );
+};
+
+export default page;
---
+++ tailwind.config.ts
@@ -0,0 +1,46 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
+  },
+  plugins: [
+    require('daisyui'),
+  ],
+};
+
+// Check if config.theme is defined before extending it
+if (config.theme) {
+  config.theme = {
+    ...config.theme,
+    extend: {
+      ...config.theme.extend,
+      keyframes: {
+        scroll: {
+          "0%": {
+            transform: "translateX(0)",
+          },
+          "100%": {
+            transform: "translateX(-100%)",
+          },
+        },
+      },
+      animation: {
+        scroll: "scroll 0.8s ease-out",
+      },
+    },
+  };
+}
+
+export default config;
